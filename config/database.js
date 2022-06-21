82c4fa2e03ca28494ae5adc2c28562453304f61b
abelseyefu <seyefuabel@gmail.com>
2022-06-20 22:34:38 -0400

    updatedproject

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/weapons', { useNewUrlParser: true,});
+console.log(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,});
 
 
 const db = mongoose.connection;
